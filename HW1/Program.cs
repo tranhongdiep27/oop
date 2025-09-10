8cfd62c2b32a1cb55cd9e37b9b0608debab608cc
tranhongdiep27 <tranhongdiep022006@gmail.com>
2025-09-10 21:16:04 +0700

    First commit clean

---
+++ HW1/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Tran Hong Diep\t12424059\t12424TN\ttranhongdiep27\ttranhongdiep022006@gmail.com");
+    }
+}
